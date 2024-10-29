06724a2ef6875d41f32c5ecbd51289aea9944b2d
mistryaksh <111272027+mistryaksh@users.noreply.github.com>
2024-10-29 13:33:53 +0530

    First Upload

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -1,254 +1,285 @@
 import { InlineCode } from "@/once-ui/components";
 
 const person = {
-    firstName: 'Selene',
-    lastName:  'Yu',
-    get name() {
-        return `${this.firstName} ${this.lastName}`;
-    },
-    role:      'Design Engineer',
-    avatar:    '/images/avatar.jpg',
-    location:  'Asia/Jakarta',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-    languages: ['English', 'Bahasa']  // optional: Leave the array empty if you don't want to display languages
-}
+  firstName: "Aakash",
+  lastName: "Mistry",
+  get name() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  role: "Full Stack Software Developer",
+  avatar: "/images/avatar.jpg",
+  location: "Asia/Kolkata", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  languages: [
+    "React",
+    "React Native",
+    "Node Js",
+    "TypeScript",
+    "Express / Koa",
+    "IOT",
+    "Web Sockets",
+    "Git / Git lab",
+    "CSS / SCSS",
+    "Android Studio & XCode",
+    "Tailwind CSS",
+    "MySQL",
+    "MongoDB",
+  ], // optional: Leave the array empty if you don't want to display languages
+};
 
 const newsletter = {
-    display: true,
-    title: <>Subscribe to {person.firstName}'s Newsletter</>,
-    description: <>I occasionally write about design, technology, and share thoughts on the intersection of creativity and engineering.</>
-}
+  display: true,
+  title: (
+    <>
+      Hi, I’m {person.firstName}! Glad to see you here. Subscribe to my
+      Newsletter
+    </>
+  ),
+  description: (
+    <>I design and code beautifully simple things, and I love what I do.</>
+  ),
+};
 
 const social = [
-    // Links are automatically displayed.
-    // Import new icons in /once-ui/icons.ts
-    {
-        name: 'GitHub',
-        icon: 'github',
-        link: 'https://github.com/once-ui-system/nextjs-starter',
-    },
-    {
-        name: 'LinkedIn',
-        icon: 'linkedin',
-        link: 'https://www.linkedin.com/company/once-ui/',
-    },
-    {
-        name: 'X',
-        icon: 'x',
-        link: '',
-    },
-    {
-        name: 'Email',
-        icon: 'email',
-        link: 'mailto:example@gmail.com',
-    },
-]
+  // Links are automatically displayed.
+  // Import new icons in /once-ui/icons.ts
+  {
+    name: "GitHub",
+    icon: "github",
+    link: "https://github.com/mistryaksh",
+  },
+  {
+    name: "LinkedIn",
+    icon: "linkedin",
+    link: "https://www.linkedin.com/in/aakash-mistry-336729182/",
+  },
+  {
+    name: "Email",
+    icon: "email",
+    link: "mailto:mistryaksh1998@gmail.com",
+  },
+];
 
 const home = {
-    label: 'Home',
-    title: `${person.name}'s Portfolio`,
-    description: `Portfolio website showcasing my work as a ${person.role}`,
-    headline: <>Design engineer and builder</>,
-    subline: <>I'm Selene, a design engineer at <InlineCode>FLY</InlineCode>, where I craft intuitive<br/> user experiences. After hours, I build my own projects.</>
-}
+  label: "Home",
+  title: `${person.name}'s Portfolio`,
+  description: `Portfolio website showcasing my work as a ${person.role}`,
+  headline: <>Full Stack Software Developer</>,
+  subline: (
+    <>
+      I'm Aksh, a Full Stack Software Developer as{" "}
+      <InlineCode>Freelancer</InlineCode>, where I Develope intuitive
+      <br /> user experiences
+    </>
+  ),
+};
 
 const about = {
-    label: 'About',
-    title: 'About me',
-    description: `Meet ${person.name}, ${person.role} from ${person.location}`,
-    tableOfContent: {
-        display: true,
-        subItems: true
-    },
-    avatar: {
-        display: true
-    },
-    calendar: {
-        display: true,
-        link: 'https://cal.com'
-    },
-    intro: {
-        display: true,
-        title: 'Introduction',
-        description: <>Selene is a Jakarta-based design engineer with a passion for transforming complex challenges into simple, elegant design solutions. Her work spans digital interfaces, interactive experiences, and the convergence of design and technology.</>
-    },
-    work: {
-        display: true, // set to false to hide this section
-        title: 'Work Experience',
-        experiences: [
-            {
-                company: 'FLY',
-                timeframe: '2022 - Present',
-                role: 'Senior Design Engineer',
-                achievements: [
-                    <>Redesigned the UI/UX for the FLY platform, resulting in a 20% increase in user engagement and 30% faster load times.</>,
-                    <>Spearheaded the integration of AI tools into design workflows, enabling designers to iterate 50% faster.</>
-                ],
-                images: [ // optional: leave the array empty if you don't want to display images
-                    {
-                        src: '/images/projects/project-01/cover-01.jpg',
-                        alt: 'Once UI Project',
-                        width: 16,
-                        height: 9
-                    }
-                ]
-            },
-            {
-                company: 'Creativ3',
-                timeframe: '2018 - 2022',
-                role: 'Lead Designer',
-                achievements: [
-                    <>Developed a design system that unified the brand across multiple platforms, improving design consistency by 40%.</>,
-                    <>Led a cross-functional team to launch a new product line, contributing to a 15% increase in overall company revenue.</>
-                ],
-                images: [ ]
-            }
-        ]
-    },
-    studies: {
-        display: true, // set to false to hide this section
-        title: 'Studies',
-        institutions: [
-            {
-                name: 'University of Jakarta',
-                description: <>Studied software engineering.</>,
-            },
-            {
-                name: 'Build the Future',
-                description: <>Studied online marketing and personal branding.</>,
-            }
-        ]
-    },
-    technical: {
-        display: true, // set to false to hide this section
-        title: 'Technical skills',
-        skills: [
-            {
-                title: 'Figma',
-                description: <>Able to prototype in Figma with Once UI with unnatural speed.</>,
-                images: [
-                    {
-                        src: '/images/projects/project-01/cover-02.jpg',
-                        alt: 'Project image',
-                        width: 16,
-                        height: 9
-                    },
-                    {
-                        src: '/images/projects/project-01/cover-03.jpg',
-                        alt: 'Project image',
-                        width: 16,
-                        height: 9
-                    },
-                ]
-            },
-            {
-                title: 'Next.js',
-                description: <>Building next gen apps with Next.js + Once UI + Supabase.</>,
-                images: [
-                    {
-                        src: '/images/projects/project-01/cover-04.jpg',
-                        alt: 'Project image',
-                        width: 16,
-                        height: 9
-                    },
-                ]
-            }
-        ]
-    }
-}
+  label: "About",
+  title: "About me",
+  description: `Meet ${person.name}, ${person.role} from ${person.location}`,
+  tableOfContent: {
+    display: true,
+    subItems: true,
+  },
+  avatar: {
+    display: true,
+  },
+  calendar: {
+    display: true,
+    link: "https://cal.com",
+  },
+  intro: {
+    display: true,
+    title: "Introduction",
+    description: (
+      <>
+        Since beginning my journey as a freelancer software developer over 4
+        years ago, I've done remote work for agencies, consulted for startups,
+        and collaborated with talented people to create digital products for
+        both business and consumer use. I'm quietly confident, naturally
+        curious, and perpetually working on improving my chops one design
+        problem at a time.
+      </>
+    ),
+  },
+  work: {
+    display: true, // set to false to hide this section
+    title: "Work Experience",
+    experiences: [
+      {
+        company: "JP Innovations Pvt Ltd",
+        timeframe: "2024 - Present",
+        role: "Seniour MERN Stack Developer",
+        achievements: [],
+        images: [],
+      },
+      {
+        company: "AlterBuddy",
+        timeframe: "2023 - 2024",
+        role: "Lead Software Developer",
+        achievements: [],
+        images: [],
+      },
+      {
+        company: "Target Peak Pvt Ltd",
+        timeframe: "2023 - 2024",
+        role: "Node js Developer",
+        achievements: [],
+        images: [],
+      },
+      {
+        company: "Sanskritect Smart Solution Pvt Ltd",
+        timeframe: "2022 - 2023",
+        role: "Trainee",
+        achievements: [],
+        images: [],
+      },
+      {
+        company: "Ansorbit Pvt Ltd",
+        timeframe: "2021",
+        role: "Internshi for 6 Months",
+        achievements: [],
+        images: [],
+      },
+    ],
+  },
+  studies: {
+    display: true, // set to false to hide this section
+    title: "Studies",
+    institutions: [
+      {
+        name: "Kadi Sarva Vishwa Vidhyalaya",
+        description: <>Bachelore in Computer Application.</>,
+      },
+    ],
+  },
+  technical: {
+    display: true, // set to false to hide this section
+    title: "Technical skills",
+    skills: [
+      {
+        title: "Node Js",
+        description: <>Able to do so next gen software server developement.</>,
+        images: [],
+      },
+      {
+        title: "React",
+        description: (
+          <>
+            Building super site & web application with gen apps with React,
+            Next.js, React Native, Electron, + Tailwindcss + Bootstap + Multi
+            Cross Plateform.
+          </>
+        ),
+        images: [],
+      },
+      {
+        title: "Mongo / SQL",
+        description: (
+          <>
+            Multi Structural application that supports multiple databaes with
+            multiverse setup.
+          </>
+        ),
+        images: [],
+      },
+    ],
+  },
+};
 
 const blog = {
-    label: 'Blog',
-    title: 'Writing about design and tech...',
-    description: `Read what ${person.name} has been up to recently`
-    // Create new blog posts by adding a new .mdx file to app/blog/posts
-    // All posts will be listed on the /blog route
-}
+  label: "Blog",
+  title: "Writing about design and tech...",
+  description: `Read what ${person.name} has been up to recently`,
+  // Create new blog posts by adding a new .mdx file to app/blog/posts
+  // All posts will be listed on the /blog route
+};
 
 const work = {
-    label: 'Work',
-    title: 'My projects',
-    description: `Design and dev projects by ${person.name}`
-    // Create new project pages by adding a new .mdx file to app/blog/posts
-    // All projects will be listed on the /home and /work routes
-}
+  label: "Work",
+  title: "My projects",
+  description: `Design and dev projects by ${person.name}`,
+  // Create new project pages by adding a new .mdx file to app/blog/posts
+  // All projects will be listed on the /home and /work routes
+};
 
 const gallery = {
-    label: 'Gallery',
-    title: 'My photo gallery',
-    description: `A photo collection by ${person.name}`,
-    // Images from https://pexels.com
-    images: [
-        { 
-            src: '/images/gallery/img-01.jpg', 
-            alt: 'image',
-            orientation: 'vertical'
-        },
-        { 
-            src: '/images/gallery/img-02.jpg', 
-            alt: 'image',
-            orientation: 'horizontal'
-        },
-        { 
-            src: '/images/gallery/img-03.jpg', 
-            alt: 'image',
-            orientation: 'vertical'
-        },
-        { 
-            src: '/images/gallery/img-04.jpg', 
-            alt: 'image',
-            orientation: 'horizontal'
-        },
-        { 
-            src: '/images/gallery/img-05.jpg', 
-            alt: 'image',
-            orientation: 'horizontal'
-        },
-        { 
-            src: '/images/gallery/img-06.jpg', 
-            alt: 'image',
-            orientation: 'vertical'
-        },
-        { 
-            src: '/images/gallery/img-07.jpg', 
-            alt: 'image',
-            orientation: 'horizontal'
-        },
-        { 
-            src: '/images/gallery/img-08.jpg', 
-            alt: 'image',
-            orientation: 'vertical'
-        },
-        { 
-            src: '/images/gallery/img-09.jpg', 
-            alt: 'image',
-            orientation: 'horizontal'
-        },
-        { 
-            src: '/images/gallery/img-10.jpg', 
-            alt: 'image',
-            orientation: 'horizontal'
-        },
-        { 
-            src: '/images/gallery/img-11.jpg', 
-            alt: 'image',
-            orientation: 'vertical'
-        },
-        { 
-            src: '/images/gallery/img-12.jpg', 
-            alt: 'image',
-            orientation: 'horizontal'
-        },
-        { 
-            src: '/images/gallery/img-13.jpg', 
-            alt: 'image',
-            orientation: 'horizontal'
-        },
-        { 
-            src: '/images/gallery/img-14.jpg', 
-            alt: 'image',
-            orientation: 'horizontal'
-        },
-    ]
-}
+  label: "Gallery",
+  title: "My photo gallery",
+  description: `A photo collection by ${person.name}`,
+  // Images from https://pexels.com
+  images: [
+    {
+      src: "/images/gallery/img-01.jpg",
+      alt: "image",
+      orientation: "vertical",
+    },
+    {
+      src: "/images/gallery/img-02.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-03.jpg",
+      alt: "image",
+      orientation: "vertical",
+    },
+    {
+      src: "/images/gallery/img-04.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-05.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-06.jpg",
+      alt: "image",
+      orientation: "vertical",
+    },
+    {
+      src: "/images/gallery/img-07.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-08.jpg",
+      alt: "image",
+      orientation: "vertical",
+    },
+    {
+      src: "/images/gallery/img-09.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-10.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-11.jpg",
+      alt: "image",
+      orientation: "vertical",
+    },
+    {
+      src: "/images/gallery/img-12.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-13.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+    {
+      src: "/images/gallery/img-14.jpg",
+      alt: "image",
+      orientation: "horizontal",
+    },
+  ],
+};
 
 export { person, social, newsletter, home, about, blog, work, gallery };
--- src/components/work/Projects.tsx
+++ src/components/work/Projects.tsx
@@ -1,38 +1,61 @@
-import { getPosts } from '@/app/utils';
-import { Flex } from '@/once-ui/components';
+import { getPosts } from "@/app/utils";
+import { Flex } from "@/once-ui/components";
 
-import { ProjectCard } from '@/components';
+import { ProjectCard } from "@/components";
+import { ProjectCardProps } from "../ProjectCard";
 
 interface ProjectsProps {
-    range?: [number, number?];
-    locale: string;
+  range?: [number, number?];
+  locale: string;
 }
 
 export function Projects({ range, locale }: ProjectsProps) {
-    let allProjects = getPosts(['src', 'app', '[locale]', 'work', 'projects', locale]);
+  let allProjects: ProjectCardProps[] = [
+    {
+      title: "AlterBuddy",
+      content:
+        "The virtual online mental therapy consulation website & mobile application",
+      description:
+        "This is my one of the best project where i used MERN stack & little bit of micro services architecture",
+      href: "https://alterbuddy.com/",
+    },
+    {
+      title: "Om Shanti Welfare Trust",
+      content:
+        "Online trust website which have most of the followers across the maharashtra",
+      description:
+        "This MERN stack web application has smoother website with animations",
+      href: "https://omshantitrust.org/",
+    },
+    {
+      title: "Jaydeep Stone Art",
+      content:
+        "Jaydeep Stone Art portfolio website for advertise their business",
+      description: "HTML & CSS Static Website",
+      href: "https://jaydeepstoneart.co.in/",
+    },
+    {
+      title: "Pooja Consultation",
+      content:
+        "Jaydeep Stone Art portfolio website for advertise their business",
+      description: "HTML & CSS Static Website",
+      href: "https://poojaconstruction.co.in/",
+    },
+    {
+      title: "Rock Economics Pvt Ltd",
+      content:
+        "Agriculture & local products brand reseller's MERN stack website.",
+      description:
+        "Online Web application for selling onine agrucultural products",
+      href: "https://rockeconomics.com",
+    },
+  ];
 
-    const sortedProjects = allProjects.sort((a, b) => {
-        return new Date(b.metadata.publishedAt).getTime() - new Date(a.metadata.publishedAt).getTime();
-    });
-
-    const displayedProjects = range
-        ? sortedProjects.slice(range[0] - 1, range[1] ?? sortedProjects.length)
-        : sortedProjects;
-
-    return (
-        <Flex
-            fillWidth gap="l" marginBottom="40" paddingX="l"
-            direction="column">
-            {displayedProjects.map((post) => (
-                <ProjectCard
-                    key={post.slug}
-                    href={`work/${post.slug}`}
-                    images={post.metadata.images}
-                    title={post.metadata.title}
-                    description={post.metadata.summary}
-                    content={post.content}
-                    avatars={post.metadata.team?.map((member) => ({ src: member.avatar })) || []}/>
-            ))}
-        </Flex>
-    );
+  return (
+    <Flex fillWidth gap="l" marginBottom="40" paddingX="l" direction="column">
+      {allProjects.map(({ ...props }, i) => (
+        <ProjectCard {...props} key={i} />
+      ))}
+    </Flex>
+  );
 }
